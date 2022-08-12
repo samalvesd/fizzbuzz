6005a0d70f0a7470faaf1381b49c155f84558105
Samuel Alves <samalves.dev@gmail.com>
2022-08-11 22:39:29 -0300

    Primeiro commit

---
+++ test/fizzbuzz_test.exs
@@ -0,0 +1,16 @@
+defmodule FizzbuzzTest do
+  use ExUnit.Case
+
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :fizzbuzz, :buzz]}
+
+      assert Fizzbuzz.build("numbers.txt") == expected_response
+    end
+    test "when an invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+
+      assert Fizzbuzz.build("invalid.txt") == expected_response
+    end
+  end
+end
